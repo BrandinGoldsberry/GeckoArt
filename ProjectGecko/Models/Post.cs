378b5bf9f9e96a7dac56a4269292ec4d4c37ef40
KingGopher <47904796+BrandinGoldsberry@users.noreply.github.com>
2020-02-21 18:23:55 -0700

    Created Post Model
    Post Model Created

---
+++ ProjectGecko/Models/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectGecko.Models
+{
+    public class Post
+    {
+        private static int _LastID;
+
+        public int PostID { get; set; }
+
+        public string Title { get; set; }
+
+        public int PosterID { get; set; }
+
+        public string[] ImagePaths { get; set; }
+
+        public List<Comment> Comments { get; set; }
+
+        public Post()
+        {
+            PostID = _LastID++;
+            Comments = new List<Comment>();
+        }
+    }
+}
