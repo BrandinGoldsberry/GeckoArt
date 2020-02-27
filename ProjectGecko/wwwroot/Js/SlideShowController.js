3c6b216529c2e3697b1a86f023a08027c83a31c9
KingGopher <47904796+BrandinGoldsberry@users.noreply.github.com>
2020-02-26 17:39:23 -0700

    Show Controller Now functions appropriately

--- ProjectGecko/wwwroot/Js/SlideShowController.js
+++ ProjectGecko/wwwroot/Js/SlideShowController.js
@@ -15,17 +15,22 @@ function plusSlides(indexer, postIndex) {
     var curPage = pageNumberHolder[postIndex];
     var curPost = postList.item(postIndex);
 
-    for (var j = 1; j < curPost.childElementCount; j++) {
-        var curImg = curPost.children.item(j);
-        curImg.style.display = "none";
-    }
-
     if (indexer < 0 && curPage > 0) {
         pageNumberHolder[postIndex] = curPage + indexer;
+        for (var j = 0; j < curPost.childElementCount; j++) {
+            var curImg = curPost.children.item(j);
+            curImg.style.display = "none";
+        }
+        curPost.children.item(pageNumberHolder[postIndex]).style.display = "block";
     }
 
-    if (indexer > 0 && pageNumberHolder[postIndex] < postList.item(postIndex).childElementCount) {
+    if (indexer > 0 && pageNumberHolder[postIndex] < postList.item(postIndex).childElementCount - 1) {
         pageNumberHolder[postIndex] = curPage + indexer;
+        for (var j = 0; j < curPost.childElementCount; j++) {
+            var curImg = curPost.children.item(j);
+            curImg.style.display = "none";
+        }
+        curPost.children.item(pageNumberHolder[postIndex]).style.display = "block";
     }
 }
 
