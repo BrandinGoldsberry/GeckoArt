931ae5279bb83d30bd13ee38323891450c9b8b28
KingGopher <47904796+BrandinGoldsberry@users.noreply.github.com>
2020-03-07 14:57:17 -0700

    ViewsCanNowBeCompikedAtRuntime

--- ProjectGecko/Startup.cs
+++ ProjectGecko/Startup.cs
@@ -17,6 +17,7 @@ namespace ProjectGecko
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(s => s.EnableEndpointRouting = false);
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
